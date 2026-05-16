e14441532f6a3d7d87b66b0bb4d49441e7554f37
Pavel Lysiankou <test@gmail.com>
2026-05-16 23:38:39 +0300

    deleted test

--- main_test.go
+++ main_test.go
@@ -17,11 +17,3 @@ func TestCalculatorSub(t *testing.T) {
 		t.Fatalf("Sub(7, 4) = %d, want 3", got)
 	}
 }
-
-//func TestCalculatorMul(t *testing.T) {
-//	c := &Calculator{}
-//
-//	if got := c.Mul(6, 5); got != 30 {
-//		t.Fatalf("Mul(6, 5) = %d, want 30", got)
-//	}
-//}
