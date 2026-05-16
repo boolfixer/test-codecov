6d89352aebb025bfa5c00b6b58b56e64e716050a
Pavel Lysiankou <test@gmail.com>
2026-05-16 23:35:49 +0300

    add unit tests

--- main_test.go
+++ main_test.go
@@ -9,3 +9,19 @@ func TestCalculatorSum(t *testing.T) {
 		t.Fatalf("Sum(2, 3) = %d, want 5", got)
 	}
 }
+
+func TestCalculatorSub(t *testing.T) {
+	c := &Calculator{}
+
+	if got := c.Sub(7, 4); got != 3 {
+		t.Fatalf("Sub(7, 4) = %d, want 3", got)
+	}
+}
+
+func TestCalculatorMul(t *testing.T) {
+	c := &Calculator{}
+
+	if got := c.Mul(6, 5); got != 30 {
+		t.Fatalf("Mul(6, 5) = %d, want 30", got)
+	}
+}
